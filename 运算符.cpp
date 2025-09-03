25bd4d164b55dc040da34775e149101bf3aa0ef7
yna25675-jpg <yna25675@gmail.com>
2025-09-03 15:19:42 +0800

    Add files via upload

---
+++ 运算符.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	//加法运算符
+	int a1=10,b1=20;
+	int result1=a1+b1;
+    printf("result1的值为: %d\n",result1);
+   	//减法运算符
+   	int a2=80,b2=28;
+   	int result2=a2-b2;
+   	printf("result2的值为: %d\n",result2);
+   	//乘法运算符
+   	int a3=4,b3=9;
+   	int result3=a3*b3;
+   	printf("result3的值为: %d\n",result3);
+   	//除法运算符
+   	float a4=90,b4=74;
+   	float result4=a4/b4;
+   	printf("result4的值为: %f\n",result4);
+	return 0;
+}
